c047a22a264d913f96b2e42c579440cdc1eaa6cd
mariquegonn-dev <ocatodev@gmail.com>
2023-08-11 19:37:40 -0300

    feat: main created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
+import "./app.css";
+import "./global.css";
+import "./sidebar.css";
+import "./main.css";
+import { Notes } from "./Components/Notes";
+
 function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div id="app">
+      <aside>
+        <strong>Caderno de Notas</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="title">Título da Anotação</label>
+            <input />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="nota">Anotações</label>
+            <textarea></textarea>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main>
+        <ul>
+          <Notes />
+        </ul>
+      </main>
+    </div>
   );
 }
 
---
+++ src/Components/Notes/Notes.jsx
@@ -0,0 +1,48 @@
+export const Notes = () => {
+  return (
+    <>
+      <li className="notepad-infos">
+        <div>
+          <strong>Fazer Compras</strong>
+          <div>x</div>
+        </div>
+
+        <textarea>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate
+          quae explicabo quaerat illo dicta unde assumenda deleniti dolore
+          cumque sunt? Architecto voluptatibus eos non quidem enim error amet
+          itaque nesciunt? lorem20
+        </textarea>
+        <span>!</span>
+      </li>
+      <li className="notepad-infos">
+        <div>
+          <strong>Fazer Compras</strong>
+          <div>x</div>
+        </div>
+
+        <textarea>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate
+          quae explicabo quaerat illo dicta unde assumenda deleniti dolore
+          cumque sunt? Architecto voluptatibus eos non quidem enim error amet
+          itaque nesciunt? lorem20
+        </textarea>
+        <span>!</span>
+      </li>
+      <li className="notepad-infos">
+        <div>
+          <strong>Fazer Compras</strong>
+          <div>x</div>
+        </div>
+
+        <textarea>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate
+          quae explicabo quaerat illo dicta unde assumenda deleniti dolore
+          cumque sunt? Architecto voluptatibus eos non quidem enim error amet
+          itaque nesciunt? lorem20
+        </textarea>
+        <span>!</span>
+      </li>
+    </>
+  );
+};
